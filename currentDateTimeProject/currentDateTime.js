3ec10fb6d86637a67ee84cbf5c86bb49d7df07ba
Matthew R. Carter <MRCarter@stpete.org>
2020-09-23 15:03:39 -0400

    update

--- currentDateTimeProject/currentDateTime.js
+++ currentDateTimeProject/currentDateTime.js
@@ -42,3 +42,43 @@ const displayDatatype = (data) => {
 }
 
 console.log(data, " is a: ", displayDatatype(data))
+
+// Function to add two numbers together
+let num1 = 10
+let num2 = 30
+
+const addTwoNumbers = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(addTwoNumbers(num1, num2))
+
+// Function that only runs when two things are true
+let bool1 = true
+let bool2 = true
+
+const runTwoTrue = (a, b) => {
+    if (a == true && b == true) {
+        return "Both items were True!!"
+    }
+}
+
+console.log(runTwoTrue(bool1, bool2))
+
+// Funtion run is one of two are true
+bool1 = false
+
+const runOneTrue = (a, b) => {
+    if (a == true || b == true) {
+        return "At least one item or both is True!!"
+    }
+}
+
+console.log(runOneTrue(bool1, bool2))
+
+// Function run with both items are false
+bool2 = false
+
+const runBothFalse = (a, b) => {
+    
+}
