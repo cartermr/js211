4d8b5b77e5e3f52e990a67e937d8c487bab79e7d
cartermr <carter.matthew@me.com>
2020-10-07 22:34:14 -0400

    update

--- checkpoints/towersOfHanoi/towersOfHanoi.js
+++ checkpoints/towersOfHanoi/towersOfHanoi.js
@@ -27,31 +27,79 @@ const printStacks = () => {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
-}
+};
 
 // Next, what do you think this function should do?
-const movePiece = () => {
-  // Your code here
-
-}
+const movePiece = (startStack, endStack) => {
+  return stacks[endStack].push(stacks[startStack].pop());
+};
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
-  // Your code here
-
-}
+const isLegal = (startStack, endStack) => {
+  // Get the piece from the start stack
+  let startValue = stacks[startStack][stacks[startStack].length - 1];
+  // Get the piece from the end stack
+  let endValue = stacks[endStack][stacks[endStack].length - 1];
+  
+  // Conpare the two pieces and return false is start is larger than end, true otherwise
+  if (startValue > endValue) {
+    return false;
+  } else {
+    return true;
+  };
+};
 
 // What is a win in Towers of Hanoi? When should this function run?
 const checkForWin = () => {
-  // Your code here
+  // Initial piece to check for is the largest
+  let value = 4;
+  // Initial win state is false
+  let win = false;
 
-}
+  // Loop through the arrays of stacks b and c
+  for (let i = 0; i < 4; i++) {
+    // Check if the piece at array[i] = value
+    if (stacks['b'][i] == value || stacks['c'][i] == value) {
+      // Decrease value to the next smaller piece
+      value -= 1;
+      // Set win state to true if the piece at array[i] keeps = to value
+      win = true;
+    } else {
+      // Set win state to false if the piece at array[i] != value
+      win = false;
+      // break out of the loop win the win state is set to fale
+      break;
+    };
+  };
+
+  // Return true or false on winniing
+  return win;
+};
 
 // When is this function called? What should it do with its argument?
 const towersOfHanoi = (startStack, endStack) => {
-  // Your code here
+  // Check that selected move was legal
+  if (isLegal(startStack, endStack)) {
+    // If isLegal returned true, then move the piece
+    movePiece(startStack, endStack);
+  };
 
-}
+  // Check if the game was won on the last move
+  if (checkForWin()) {
+    // Display a winning message
+    console.log('************************************');
+    console.log('Congrats! You won Towers of Hanoi!');
+    printStacks();
+    console.log('************************************');
+    // Start new game and reset board
+    console.log('\nNew Game\n');
+    stacks = {
+      a: [4, 3, 2, 1],
+      b: [],
+      c: []
+    };
+  };
+};
 
 
 const getPrompt = () => {
@@ -62,47 +110,54 @@ const getPrompt = () => {
       getPrompt();
     });
   });
-}
+};
 
 // Tests
-if (typeof describe === 'function') {
+// Run test functions if run through npm test
+if (typeof describe == 'function') {
 
-  describe('#towersOfHanoi()', () => {
-    it('should be able to move a block', () => {
+  // Check that player can actually move a piece on the board
+  describe('Is the game playable', () => {
+    it('Player should be able to move a piece on the stacks', () => {
+      // Play a round of game
       towersOfHanoi('a', 'b');
-      assert.deepEqual(stacks, { a: [4, 3, 2], b: [1], c: [] });
+      // Check that the piece moved on the board
+      assert.deepStrictEqual(stacks, {a: [4, 3, 2], b: [1], c: []});
     });
   });
 
-  describe('#isLegal()', () => {
-    it('should not allow an illegal move', () => {
-      stacks = {
-        a: [4, 3, 2],
-        b: [1],
-        c: []
-      };
-      assert.equal(isLegal('a', 'b'), false);
+  // Check the legality of the move by the player
+  describe('Did the player make a legal move', () => {
+    it('Will not allow an illegal move', () => {
+      // Set the game board
+      stacks = {a: [4, 3, 2], b: [1], c: []};
+      // Make an illegal moved, 2 on top of 1
+      assert.ok(!isLegal('a', 'b'));
     });
-    it('should allow a legal move', () => {
-      stacks = {
-        a: [4, 3, 2, 1],
-        b: [],
-        c: []
-      };
-      assert.equal(isLegal('a', 'c'), true);
-    });
-  });
-  describe('#checkForWin()', () => {
-    it('should detect a win', () => {
-      stacks = { a: [], b: [4, 3, 2, 1], c: [] };
-      assert.equal(checkForWin(), true);
-      stacks = { a: [1], b: [4, 3, 2], c: [] };
-      assert.equal(checkForWin(), false);
+    it('Will allow a legal move', () => {
+      // Set the game board
+      stacks = {a: [4, 3, 2, 1], b: [], c: []};
+      // Make a legal move, 1 to an empty stack
+      assert.ok(isLegal('a', 'c'));
     });
   });
 
+  // Check for a winning game
+  describe('Check if the player won the game', () => {
+    it('Check for winning game', () => {
+      // Set stacks as a winning game
+      stacks = {a: [], b: [4, 3, 2, 1], c: []};
+      // Check that checkForWin return true
+      assert.ok(checkForWin());
+    });
+    it('Determine the player has not won the game yet', () => {
+      // Set stacks as a non-winning game
+      stacks = {a: [4, 3], b: [1], c: [2]};
+      // Check that checkForWin returns false
+      assert.ok(!checkForWin());
+    });
+  });
 } else {
-
+  // Start game if run through node
   getPrompt();
-
-}
+};
